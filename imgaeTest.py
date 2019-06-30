a4c4aa82d7dd8c4b43fc5c890c2e20eb7193ac33
SeanDuback <sean.duback@gmail.com>
2019-06-29 18:10:27 -0600

    Initial Commit

---
+++ imgaeTest.py
@@ -0,0 +1,27 @@
+""" from PIL import Image, ImageDraw
+
+im = Image.open('C:/Users/seand/Desktop/Test.png')
+draw = ImageDraw.Draw(im)
+draw.line((0, 0) + im.size, fill=128)
+draw.line((0, im.size[1], im.size[0], 0), fill=128)
+del draw
+
+# write to stdout
+im.save(sys.stdout, "PNG")
+# im.show() 
+ """
+
+from PIL import Image, ImageDraw
+import sys, pyautogui, time 
+
+im = Image.open('Test.jpg')
+draw = ImageDraw.Draw(im)
+#Draw a rectangle to cover "Test"
+""" for x in range(500):
+    for y in range(300):
+        draw.point((x,y), fill=128) """
+draw.point((100,100), fill=128)
+del draw
+# write to stdout
+im.save(sys.stdout.encoding, "PNG")
+im.show()

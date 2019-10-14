b922de9f6446bd566fdd8561fe4b122fe965b23a
sean duback <sean.duback@gmail.com>
2019-10-14 10:04:04 -0600

    TESTED WORKING start code, also added communicate with server py code, NOT WORKING score, try numpy.allclose

--- LearnToWrite.py
+++ LearnToWrite.py
@@ -26,19 +26,19 @@ letter = cv2.imread('bw_image.png', 1)
 pixDistance = 0
 pixDistFirst = []
 pixDistSecond = []
+pixDistStart = 100
 minPixDist = {}
 # grab the referenceto the webcam
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(1)
 # define the lower and upper boundaries of the "green"
 colorLower = (0, 120, 70)
 colorUpper = (50, 255, 180)
 winNum = 0
-tLCorner = (350, 160)
-bLCorner = (350, 500)
-tRCorner = (450, 160)
-bRCorner = (450, 500)
+
 startFlag = False
 oldStartFlag = False 
+endFlag = False 
+score = 0
 
 ############################################################# Class Definitions #################################################
 
@@ -50,20 +50,6 @@ class Dwin(object):
         self.xmin = xmin
         self.xmax = xmax
 
-# class oldXY(object):
-#     def __init__(self, x0=0, y0=0, x1=0, y1=0, x2=0, y2=0, x3=0, y3=0, x4=0, y4=0,):
-#         self.x0 = x0
-#         self.y0 = y0
-#         self.x1 = x1
-#         self.y1 = y1
-#         self.x2 = x2
-#         self.y2 = y2
-#         self.x3 = x3
-#         self.y3 = y3
-#         self.x4 = x4
-#         self.y4 = y4
-
-
 
 ############################################################# Functions ###########################################
 
@@ -73,54 +59,61 @@ def distance(x0, y0, x1, y1):
 def start (img, inputX, inputY):
     
     for x in range(dWinList[0].xmin, dWinList[0].xmax):
-        # print(float(inputX))
-        # if inputX == 336:
-        #     return True
-        if x == int(inputX):
-            for y in range(dWinList[0].ymin, dWinList[0].ymax, -1):
-                if y == int(inputY):
-                    if np.array_equal(img[y, x], black):
-                        return True
+        for y in range(dWinList[0].ymin, dWinList[0].ymax, -1):
+            if np.array_equal(img[y, x], black):
+                pixDistStart = int(distance(x, y, inputX, inputY))
+                if pixDistStart <= 5:
+                    return True
     return False
 
 
 def getMinDistance (img, dWinNum, inputX, inputY):
-    countFirst = 0
-    countSecond = 0
-
-    # for Dwin in dWinList:
-    #     if Dwin.idnum == dWinNum:
-    for x in range(dWinList[dWinNum].xmin, dWinList[dWinNum].xmax):
-        if x == int(inputX): #inputX and x same type ???
-            for y in range(dWinList[dWinNum].ymin, dWinList[dWinNum].ymax, -1):
-                if y == int(inputY):
-                    if np.array_equal(img[y, x], black):
-                        countFirst += 1
-                        pixDistFirst.insert(countFirst, distance(x, y, int(inputX), int(inputY)))
-        
-    #elif Dwin.idnum == (dWinNum+1):
-    for x in range(dWinList[dWinNum+1].xmin, dWinList[dWinNum+1].xmax):
-        if x == int(inputX):
-            for y in range(dWinList[dWinNum+1].ymin, dWinList[dWinNum+1].ymax, -1):
-                if y == int(inputY):
-                    if  np.array_equal(img[y, x],black):
-                        countSecond += 1
-                        pixDistSecond.insert(countSecond, distance(x, y, int(inputX), int(inputY)))
+    FirstKey = 0 #used as flags for distance to not update min distince if no new values
+    SecondKey = 0 #used as flags for distance to not update min distince if no new values
+    global score
+    oldScore = score
     
-    if countFirst != 0: minPixDist.update({dWinNum: min(pixDistFirst)})
-    if countSecond != 0: minPixDist.update({dWinNum+1: min(pixDistSecond)})
-    if countFirst != 0 or countSecond != 0:  
+    #print("DwinNum = %s"%(dWinNum))
+    if dWinNum == 36: #if the game is over return end game true
+        return 36, 0, True
+    else:
+        for x in range(dWinList[dWinNum].xmin, dWinList[dWinNum].xmax):
+            for y in range(dWinList[dWinNum].ymin, dWinList[dWinNum].ymax, -1):
+                if np.array_equal(img[y, x], black):
+                    if np.array_equal(img[y,x], [inputY, inputX]): #if the input is on a letter pixel add one to score and return
+                           score = oldScore +1
+                           return dWinNum, 0, False
+                    else:    
+                        FirstKey += 1 #Counts up as the loop goes on so every value has a unquie key in the dict
+                        pixDistFirst.insert(FirstKey, distance(x, y, inputX, inputY))
 
+        for x in range(dWinList[dWinNum+1].xmin, dWinList[dWinNum+1].xmax):
+            for y in range(dWinList[dWinNum+1].ymin, dWinList[dWinNum+1].ymax, -1):
+                if  np.array_equal(img[y, x],black):
+                    if np.array_equal(img[y,x], [inputY, inputX]): #if the input is on a letter pixel add one to score and return
+                           score = oldScore +1
+                           return dWinNum+1, 0, False 
+                    else:
+                        SecondKey += 1 #Counts up as the loop goes on so every value has a unquie key in the dict
+                        pixDistSecond.insert(SecondKey, distance(x, y, inputX, inputY))
+        
+        if FirstKey != 0: minPixDist.update({dWinNum: int(min(pixDistFirst))})
+        else: minPixDist[dWinNum] = 1000
+        if SecondKey != 0: minPixDist.update({dWinNum+1: int(min(pixDistSecond))})
+        else: minPixDist[dWinNum+1] = 1000
+        #if FirstKey != 0 or SecondKey != 0:  #if a black pixel was found, this will always be true unless FUBAR 
         if minPixDist[dWinNum+1] < minPixDist[dWinNum]:
             print ("Window#= %s, Distance from Input to nearest letter pixel = %s"% (dWinNum+1, minPixDist[dWinNum+1]))
             pixDistFirst.clear()
             pixDistSecond.clear()
-            return dWinNum+1, minPixDist[dWinNum+1]
-        else:
+            return dWinNum+1, minPixDist[dWinNum+1], False 
+        elif minPixDist[dWinNum] < 1000:
             print ("Window#= %s, Distance from Input to nearest letter pixel = %s"% (dWinNum, minPixDist[dWinNum]))
             pixDistFirst.clear()
             pixDistSecond.clear()
-            return dWinNum, minPixDist[dWinNum]
+            return dWinNum, minPixDist[dWinNum], False
+        else:
+            return dWinNum, 0, False
 
 def colorDwin (img, class_type="Dwin"):
     #func used for debugging 
@@ -134,29 +127,8 @@ def colorDwin (img, class_type="Dwin"):
                 # else:
                 #     img[y, x] = 0
 
-
-# def shortTail(img, centerX, centerY):
-#     oldXY.x0 = centerX
-#     oldXY.y0 = centerY
-#     oldXY.x1 = oldXY.x0
-#     oldXY.y1 = oldXY.y0
-#     oldXY.x2 = oldXY.x1
-#     oldXY.y2 = oldXY.y1
-#     oldXY.x3 = oldXY.x2
-#     oldXY.y3 = oldXY.y2
-#     oldXY.x4 = oldXY.x3
-#     oldXY.y4 = oldXY.y3
-
-#     cv2.circle(img, (centerX, centerY), 1, (0, 0, 255), -1)
-#     cv2.circle(img, (oldXY.x0, ), 1, (0, 0, 255), -1)
-#     cv2.circle(img, (centerX, centerY), 1, (0, 0, 255), -1)
-#     cv2.circle(img, (centerX, centerY), 1, (0, 0, 255), -1)
-#     cv2.circle(img, (centerX, centerY), 1, (0, 0, 255), -1)
-#     cv2.circle(img, (centerX, centerY), 1, (0, 0, 255), -1)
-
 def init ():
     #make Distance windows
-    #def __init__(self, idnum=0, ymin=0, ymax=0, xmin=0, xmax=0):
     dWinList.append(Dwin(0, 421, 404, 366, 374))
     dWinList.append(Dwin(1, 404, 402, 372, 376))
     dWinList.append(Dwin(2, 402, 396, 374, 378))
@@ -197,12 +169,7 @@ def init ():
     dWinList.append(Dwin(33, 475, 455, 410, 430))
     dWinList.append(Dwin(34, 455, 435, 420, 440))
     dWinList.append(Dwin(35, 435, 405, 430, 450))
-
-    # #Create square boarder around letter
-    # cv2.line(letter, tLCorner, bLCorner, 5)
-    # cv2.line(letter, tRCorner, tLCorner, 5)
-    # cv2.line(letter, tRCorner, bRCorner, 5)
-    # cv2.line(letter, bRCorner, bLCorner, 5)
+    dWinList.append(Dwin(36, 0, 0, 0, 0)) #dummy window for dWin +1 loop when dWin loop is on dwin=35
 
 init()
 #colorDwin(letter, dWinList)
@@ -210,13 +177,7 @@ while True:
     #ArduinoBT = serial.Serial("COM3",9600,timeout = 2)
     cv2.imshow("Learn to Write!", letter)
     key = cv2.waitKey(1) & 0xFF
-
-    # if the q key is pressed, stop the loop
-    if key == ord("q"):
-        break
-
-# while True:
-   
+ 
     # grab the current frame
     (grabbed, frame) = camera.read()
     frame = cv2.flip(frame, 1)
@@ -225,116 +186,48 @@ while True:
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # construct a mask for the color then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
+    # construct a mask for the color then perform a series of dilations and erosions to remove any small blobs left in the mask
     mask = cv2.inRange(hsv, colorLower, colorUpper)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
 
-    # find contours in the mask and initialize the current
-    # (x, y) center of the ball
+    # find contours in the mask and initialize the current (x, y) center of the ball
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-    center = None
+    center = None #why is center none?
 
     # only proceed if at least one contour was found
     if len(cnts) > 0:
-        # find the largest contour in the mask, then use
-        # centroid
+        # find the largest contour in the mask, then use centroid
         c = max(cnts, key=cv2.contourArea)
         ((x, y), radius) = cv2.minEnclosingCircle(c)
         M = cv2.moments(c)
         centerX = (int(M["m10"] / M["m00"])+50)
         centerY = (int(M["m01"] / M["m00"])+50)
+        #print("X is %s, Y is %s"%(int(centerX),int(centerY)))
         if centerX < 800 and centerY < 600: 
-
-            winNum, pixDistance = getMinDistance(letter, winNum, x, y)
-            cv2.circle(letter, (centerX, centerY), 1, (0, 0, 255), -1)
-
-
-            # if startFlag == False: #Wait until imput is in Dwin 0 to start game
-            #     cv2.circle(letter, (centerX, centerY), 1, (0, 0, 255), -1)
-            #     startFlag = start(letter, x, y)
-            # else:
-            #     if oldStartFlag == False: #Clean the img on game start 
-            #         oldStartFlag = True 
-            #         letter = cv2.imread('bw_image.png', 1)
-            #         winNum, pixDistance = getMinDistance(letter, winNum, x, y)
-            #         cv2.circle(letter, (centerX, centerY), 1, (0, 0, 255), -1)
-            #     else: #Play the game
-            #         winNum, pixDistance = getMinDistance(letter, winNum, x, y)
-            #         cv2.circle(letter, (centerX, centerY), 1, (0, 0, 255), -1)
+            if startFlag == False: #Wait until input is near Dwin 0 to start game
+                cv2.circle(letter, (centerX, centerY), 1, (0, 0, 255), -1)
+                startFlag = start(letter, centerX,centerY)
+            else:
+                if oldStartFlag == False: #Clean the img oqn game start 
+                    oldStartFlag = True 
+                    letter = cv2.imread('bw_image.png', 1)
+                    winNum, pixDistance, endFlag = getMinDistance(letter, winNum, centerX, centerY)
+                    cv2.circle(letter, (centerX, centerY), 1, (0, 0, 255), -1)
+                else: #Play the game
+                    winNum, pixDistance, endFlag = getMinDistance(letter, winNum, centerX, centerY)
+                    cv2.circle(letter, (centerX, centerY), 1, (0, 0, 255), -1)
                     #send distance to arduino here!
                     #ArduinoBT.write(pixDistance)
-                        #windows code options  
-               
-                            #https://stackoverflow.com/questions/36503170/python-communication-to-arduino-via-bluetooth
-                            # ArduinoBT.write(bytes("hello!",'utf-8'))
-                            #code uses HC-06 bluetooth module
-
-                            #https://github.com/huberf/Computer-to-Arduino-Bluetooth/blob/master/LightGUI.py
-                            # sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-                            # def disconnect(self):
-                            #     #Close socket connection to device
-                            #     self.sock.close()
-                            # def on(self):
-                            #     #Send 'H' which the Arduino
-                            #     #detects as turning the light on
-                            #     data = "H"
-                            #     self.sock.send(data)
-                        #Pi code options 
-                            #http://www.uugear.com/portfolio/bluetooth-communication-between-raspberry-pi-and-arduino/
-                            # /usr/bin/python
-                            # import serial
-                            # from time import sleep
-                            # bluetoothSerial = serial.Serial( "/dev/rfcomm1", baudrate=9600 )
-                            # count = None
-                            # while count == None:
-                            #     try:
-                            #         count = int(raw_input( "Please enter the number of times to blink the L$
-                            #     except:
-                            #         pass    # Ignore any errors that may occur and try again
-
-                            #Arduino code
-                                # int ledPin = 13;
-                                # void setup() {
-                                #   Serial.begin( 9600 );    // 9600 is the default baud rate for the serial Bluetooth module
-                                # }
-                                # void loop() {
-                                #   // listen for the data
-                                #   if ( Serial.available() > 0 ) {
-                                #     // read a numbers from serial port
-                                #     int count = Serial.parseInt();
-                                #      // print out the received number
-                                #     if (count > 0) {
-                                #         Serial.print("You have input: ");
-                                #         Serial.println(String(count));
-                                #         // blink the LED
-                                #         blinkLED(count);
-                                #     }
-                                #   }
-                                # }
-                                # void blinkLED(int count) {
-                                #   for (int i=0; i< count; i++) {
-                                #     digitalWrite(ledPin, HIGH); 
-                                #     delay(500);
-                                #     digitalWrite(ledPin, LOW);
-                                #     delay(500);
-                                #   } 
-                                # }
-                                # bluetoothSerial.write( str(count) )
-                                # print bluetoothSerial.readline()          
-                        # https://medium.com/@mahesh_joshi/raspberry-pi-3-and-arduino-communication-via-bluetooth-hc-05-5d7b6f162ab3     
-                        # http://blog.whatgeek.com.pt/2015/09/bluetooth-communication-between-raspberry-pi-and-arduino/      
 
     cv2.imshow("Learn to Write!", letter)
     key = cv2.waitKey(1) & 0xFF
 
     # if the q key is pressed, stop the loop
-    if key == ord("q"):
+    if key == ord("q") or endFlag == True:
+        print("Your Score is %s letter pixels hit out of 611 letter pixels"%(score))
         break
 
-# if the q key is pressed, stop the loop
 camera.release()
 cv2.destroyAllWindows()
 
---
+++ mqttTest.py
@@ -0,0 +1,55 @@
+import paho.mqtt.client as mqtt
+import time
+
+def on_log(client, userdata, level, buf):
+    print("log: "+buf)
+
+def on_connect(client, userdata, flags, rc):
+    if rc==0:
+        print("connected OK")
+    else:
+        print("bad connection returned code=",rc)
+
+
+broker= "localhost" #"test.mosquitto.org"
+
+client = mqtt.Client("ESP32")
+
+client.on_connect=on_connect
+client.on_log=on_log
+print("connecting to broker",broker)
+client.connect(broker)
+f=0
+l=0
+b=0
+r=0
+cycle=0
+
+while True:
+    if cycle==1:
+        f=1
+        l=0
+        b=0
+        r=0
+    elif cycle==2:
+        f=0
+        l=1
+        b=0
+        r=0
+    elif cycle==3:
+        f=0
+        l=0
+        b=1
+        r=0
+    elif cycle==4:
+        f=0
+        l=0
+        b=0
+        r=1
+        cycle=0
+    
+    msg = ("{0},{1},{2},{3}".format(f,l,b,r))
+    client.publish("motors",msg)
+    cycle += 1
+    time.sleep(4)
+client.loop_forever()
